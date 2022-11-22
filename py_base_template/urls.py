48e3f8fa09f1308d078a670c7513262c592936cb
lazzy <565260908lazzysun@gmail.com>
2022-11-22 16:45:32 +0800

    移除admin管理路由

--- py_base_template/urls.py
+++ py_base_template/urls.py
@@ -10,14 +10,12 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('v1/', include("apps.login.urls")),
     path('v2/', include("apps.org.urls")),
 ]
